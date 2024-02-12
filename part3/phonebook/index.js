52b68f47b71c730611ed38943de6a779ae99e505
keyeser <lucadbe@proton.me>
2024-02-12 23:12:44 +0100

    Added cors

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,15 +1,18 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const morgan = require('morgan');
 
 const PORT = process.env.PORT || 4000;
 
-const phonebook = [
+let phonebook = [
     { id: 1, name: "Arto Hellas", number: "040-123456" },
     { id: 2, name: "Ada Lovelace", number: "39-44-5323523" },
     { id: 3, name: "Dan Abramov", number: "12-43-234345" },
     { id: 4, name: "Mary Poppendieck", number: "39-23-6423122" }
 ];
+
+app.use(cors());
 
 app.use(morgan('tiny'));
 
