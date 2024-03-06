351ba235db81adad073d3035917db7c7ce31093d
keyeser <lucadbe@proton.me>
2024-03-06 18:36:23 +0100

    Edit notebook

--- part3/notes/backend/index.js
+++ part3/notes/backend/index.js
@@ -38,7 +38,7 @@ app.get('/api/notes', (request, response) => {
   });
 });
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body;
 
   if (body.content === undefined) {
@@ -51,8 +51,9 @@ app.post('/api/notes', (request, response) => {
   });
 
   note.save().then(savedNote => {
-    response.json(savedNote);
-  });
+    response.json(savedNote)
+  })
+  .catch(error => next(error));
 });
 
 app.get('/api/notes/:id', (request, response, next) => {
@@ -76,19 +77,20 @@ app.delete('/api/notes/:id', (request, response, next) => {
 });
 
 app.put('/api/notes/:id', (request, response, next) => {
-  const body = request.body;
 
-  const note = {
-    content: body.content,
-    important: body.important,
-  };
+  const { content, important } = request.body
 
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+  Note.findByIdAndUpdate(
+    request.params.id, 
+
+    { content, important },
+    { new: true, runValidators: true, context: 'query' }
+  ) 
     .then(updatedNote => {
-      response.json(updatedNote);
+      response.json(updatedNote)
     })
-    .catch(error => next(error));
-});
+    .catch(error => next(error))
+})
 
 // Middleware per endpoint sconosciuto
 const unknownEndpoint = (request, response) => {
@@ -102,7 +104,9 @@ const errorHandler = (error, request, response, next) => {
   console.error(error.message);
 
   if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' });
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error);
